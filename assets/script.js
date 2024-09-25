ec74fe8912206de377881296361a8eaba0c593e1
dngst <dngst@protonmail.com>
2024-09-25 21:57:42 +0300

    Remove submit btn

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,31 @@
-async function fetchCSV(filePath) {
-    const response = await fetch(filePath);
-    const csvText = await response.text();
-    return parseCSV(csvText);
-}
+const fetchCSV = async (filePath) => {
+  const response = await fetch(filePath);
+  const csvText = await response.text();
+  return parseCSV(csvText);
+};
 
-function parseCSV(csv) {
-    const rows = csv.split('\n').map(row => row.split(','));
-    const headers = rows.shift();
+const parseCSV = (csv) => {
+  const rows = csv.split("\n").map((row) => row.split(","));
+  const headers = rows.shift();
 
-    return rows.map(row => {
-        const yearLocation = row[0];
-        const cyclist = row[1];
-        return { yearLocation, cyclist };
-    });
-}
+  return rows.map(([yearLocation, cyclist]) => ({ yearLocation, cyclist }));
+};
 
-function searchByYear(data, year) {
-    const results = data.filter(row => row.yearLocation.startsWith(year.toString()));
-    return results.length > 0
-        ? results.map(result => `${result.yearLocation}, ${result.cyclist}`).join('\n')
-        : 'No results found';
-}
+const searchByYear = (data, year) => {
+  const results = data.filter((row) =>
+    row.yearLocation.startsWith(year.toString()),
+  );
+  return results.length > 0
+    ? results
+        .map((result) => `${result.yearLocation}, ${result.cyclist}`)
+        .join("\n")
+    : "No results found";
+};
 
-document.getElementById('form').addEventListener('submit', async function(e) {
-    e.preventDefault();
-    const year = document.getElementById('year').value;
-    const data = await fetchCSV('data/cyclists.csv');
-    const result = searchByYear(data, year);
-    document.getElementById('result').innerText = result;
+document.getElementById("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const year = document.getElementById("year").value;
+  const data = await fetchCSV("data/cyclists.csv");
+  const result = searchByYear(data, year);
+  document.getElementById("result").innerText = result;
 });
-
